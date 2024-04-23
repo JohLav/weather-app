5ffba8e52ede3e1e9e8005d5c031d540f145d189
Johana Lavigne <johana.lavigne@gmail.com>
2024-04-23 17:06:55 +0200

    Feat: favorite a city using useState

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,6 +5,7 @@ import fetchAPI from "../services/fetchAPI.js";
 export const Weather = () => {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState({});
+  const [isFavorite, setIsFavorite] = useState(city.isFavorite);
 
   const handleInputChange = (event) => setCity(event.target.value);
 
@@ -13,44 +14,65 @@ export const Weather = () => {
     fetchAPI(city, setWeatherData);
   };
 
+  const handleClickFavorite = () => {
+    event.preventDefault();
+    setIsFavorite(!isFavorite);
+  };
+
   const weatherIcon = weatherData.weather
     ? `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
     : null;
 
   return (
     <>
-      <div className="weatherDetails">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter city name"
-            value={city}
-            onChange={handleInputChange}
-          />
-          <button type="submit">Get Weather</button>
-        </form>
-        {weatherData.main && (
-          <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={handleInputChange}
+        />
+        {/*<button type="submit">Get Weather</button>*/}
+      </form>
+      {weatherData.main && (
+        <>
+          <div className="title">
             <h2>{city}</h2>
-            <div className="header">
-              {weatherIcon && (
-                <img
-                  src={weatherIcon}
-                  alt={weatherData.weather[0].description}
-                />
-              )}
-              <div className="main-data">
-                <span className="temperature">{weatherData.main.temp}°C</span>
-                <span>{weatherData.weather[0].description}</span>
-              </div>
+            <button
+              id="favorite"
+              onClick={handleClickFavorite}
+              className={!isFavorite ? "notFavorite" : "isFavorite"}
+            ></button>
+          </div>
+          <div className="header">
+            {weatherIcon && (
+              <img src={weatherIcon} alt={weatherData.weather[0].description} />
+            )}
+            <div className="main-data">
+              <span>{weatherData.main.temp}°C</span>
+              <p>{weatherData.weather[0].description}</p>
             </div>
-            <p className="info">Feels like: {weatherData.main.feels_like}°C</p>
-            <p className="info">Humidity: {weatherData.main.humidity}%</p>
-            <p className="info">Pressure: {weatherData.main.pressure}</p>
-            <p className="info">Wind speed: {weatherData.wind.speed}m/s</p>
-          </>
-        )}
-      </div>
+          </div>
+          <div className="container-el">
+            <div className="element">
+              <span>{weatherData.main.feels_like}°C</span>
+              <p>feels like</p>
+            </div>
+            <div className="element">
+              <span>{weatherData.main.humidity}%</span>
+              <p>humidity</p>
+            </div>
+            <div className="element">
+              <span>{weatherData.main.pressure}</span>
+              <p>pressure</p>
+            </div>
+            <div className="element">
+              <span>{weatherData.wind.speed}m/s</span>
+              <p>wind speed</p>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
